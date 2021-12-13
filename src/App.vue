97fe3f80843159fee509809d1f51e69b81459686
github-laziji <c_smtp@163.com>
2021-12-13 23:27:18 +0800

    add element+

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 
 
 <template>
-  <div>
+
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + Vite" />
-  </div>
+      <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button>中文</el-button>
+ 
 </template>
 
 <script >
