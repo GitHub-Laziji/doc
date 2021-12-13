907fc5bf9e7a5b4474f36cb9de1d6f5e38e4fb70
github-laziji <c_smtp@163.com>
2021-12-13 22:55:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    {{ aaa }}
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+</template>
+
+<script >
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components:{
+    HelloWorld
+  },
+  data() {
+    // 暴露给 template
+    return {
+      aaa: "aaaasdd"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
