34f77fe7c6a13bb2222effb10c8a74b246e4c1e0
github-laziji <c_smtp@163.com>
2021-12-18 14:27:01 +0800

    +

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,94 @@
 
 
 <template>
-  <div class="header">
-    <div style="display: flex;align-items: center;">
-      <img src="./assets/switch3.svg" style="height:80px">
-      <span>Laziji</span>
-      / Laziji
+  <div :class="['day', 'night'][theme]">
+    <div class="header" :class="[smHead ? 'header-sm' : '']">
+      <div>
+        <img src="./assets/fb3.svg" />
+        <div class="name">Laziji</div>
+        <div class="desc">&nbsp;/&nbsp;Designer</div>
+      </div>
+      <div>
+        <div class="item">作品集</div>
+        <div class="item">关于我</div>
+        <el-switch
+          v-model="theme"
+          inline-prompt
+          :active-icon="icon.Sunny"
+          :inactive-icon="icon.Moon"
+          :inactive-value="0"
+          :active-value="1"
+          active-color="#303133"
+          inactive-color="#303133"
+          border-color="#303133"
+          width="42"
+        ></el-switch>
+      </div>
     </div>
-    <div style="display: flex;">
-      <div style="font-size: 1.55rem;line-height: 1.3;">关于我</div>
+    <div class="bk">
+      <div class="img">Laziji</div>
+      <div class="btm"></div>
     </div>
-  </div>
-  <div class="bk">
-    <div class="img">
-      Laziji
+    <div style="padding: 40px 0;display: flex;justify-content: center;">
+      <el-button type="primary" round style="width:240px">Primary</el-button>
     </div>
-    <div class="btm"></div>
-  </div>
-  <div style="padding: 40px 0;display: flex;justify-content: center;">
-    <el-button type="primary" round style="width:240px">Primary</el-button>
+    <div style="height: 800px;"></div>
   </div>
 </template>
 
 <script >
 import HelloWorld from "./components/HelloWorld.vue";
-
+import { Sunny, Moon } from '@element-plus/icons-vue'
 export default {
   components: {
     HelloWorld
   },
   data() {
     return {
-      loading: true
+      icon: { Sunny, Moon },
+      theme: 0,
+      loading: true,
+      smHead: false
     }
   },
   mounted() {
     fetch("./doc/content.json").then(resp => resp.json()).then(data => {
       console.log(data)
-    })
+    });
+    window.onscroll = () => {
+      let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      this.smHead = scrollTop > 200;
+    }
   }
 }
 </script>
 
-<style scoped>
+<style  lang="less" scoped>
+@header-bk: #ffffffaa;
+
+.night {
+  background: #000;
+  .header {
+    background: #000000aa;
+    div{
+      color: #fff !important;
+    }
+  }
+  .bk {
+    .img {
+      background-color: #9791cd;
+      color: #fff !important;
+    }
+    .btm {
+      background-image: linear-gradient(#ffffff00, #000);
+    }
+  }
+}
 .header {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  height: 72px;
   z-index: 1000;
   background: #ffffffaa;
   transition: all 0.4s ease;
@@ -56,45 +96,131 @@ export default {
   align-items: center;
   justify-content: space-between;
   padding: 0 16px;
+  img {
+    transition: all 0.4s ease;
+  }
+  .name {
+    color: #303133;
+    transition: all 0.4s ease;
+    margin-left: 16px;
+    font-weight: 600;
+  }
+  .desc {
+    transition: all 0.4s ease;
+    font-weight: 600;
+    color: #909399;
+  }
+  > div:first-child {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    transition: all 0.4s ease;
+  }
+  > div:last-child {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    transition: all 0.4s ease;
+    margin-right: 30px;
+    .item {
+      padding: 0 30px;
+      cursor: pointer;
+      transition: all 0.4s ease;
+      border-bottom: 4px solid #00000000;
+      border-top: 4px solid #00000000;
+      margin-right: 10px;
+      font-weight: 600;
+      color: #303133;
+      &:hover {
+        padding-top: 18px;
+        padding-bottom: 18px;
+        color: #409eff;
+        border-bottom: 4px solid #409eff;
+      }
+    }
+  }
 }
 .bk {
   width: 100%;
-  height: 700px;
   position: relative;
+  .img {
+    background-image: url(./assets/fj2.png);
+    background-color: #add5fd;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    height: 100%;
+    width: 100%;
+    position: relative;
+    background-position: center center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    // justify-content: center;
+    color: #303133;
+    font-weight: 600;
+    font-size: 20vw;
+    padding-left: 10vw;
+  }
+  .btm {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    z-index: 10;
+    height: 120px;
+    width: 100%;
+    background-image: linear-gradient(#ffffff00, #ffffff);
+  }
 }
-.bk .img {
-  background-image: url(./assets/bk.jpg);
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  height: 100%;
-  width: 100%;
-  position: relative;
-  background-position: center center;
-  background-size: cover;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color:#303133;
-  font-weight: 600;
-  font-size: 10rem;
-}
-.bk .btm {
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  z-index: 10;
-  height: 120px;
-  width: 100%;
-  background-image: linear-gradient(#ffffff00, #ffffff);
+@media (min-width: 576px) {
 }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+// Medium devices (tablets, 768px and up)
+@media (min-width: 768px) {
+}
+
+// Large devices (desktops, 992px and up)
+@media (min-width: 992px) {
+}
+
+// Extra large devices (large desktops, 1200px and up)
+@media (min-width: 1200px) {
+  .header {
+    height: 98px;
+    img {
+      height: 70px;
+    }
+    .name {
+      font-size: 1.3rem;
+    }
+    .desc {
+      font-size: 1.3rem;
+    }
+    > div:last-child div {
+      font-size: 1.55rem;
+      line-height: 1.3;
+      letter-spacing: 0.025rem;
+    }
+  }
+  .header-sm {
+    height: 72px;
+    img {
+      height: 56px;
+    }
+    .name {
+      font-size: 1.2rem;
+    }
+    .desc {
+      font-size: 1.2rem;
+    }
+    > div:last-child div {
+      font-size: 1.35rem;
+      line-height: 1.2;
+      letter-spacing: 0.025rem;
+    }
+  }
+  .bk {
+    height: 700px;
+  }
 }
 </style>
 <style>
@@ -104,5 +230,7 @@ body {
 }
 * {
   box-sizing: border-box;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
 }
 </style>
